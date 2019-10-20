7f14be076254438c608e04b351a42eaab36d8576
Xin Yuan <XinYuan1615@gmail.com>
2019-10-20 15:00:45 +0800

    Chapter 22

---
+++ Chapter 22 R graphics landscape.R
@@ -0,0 +1,20 @@
+# 要点
+#    详情见Chapter 22 ggplot2-tutorial-slides.pdf
+
+# 主要函数:
+
+##### Chapter 22 R graphics landscape #####
+#  A picture is worth a thousand words
+
+
+# use data.frames
+# use factors
+# be the boss of your factors
+# keep your data tidy
+# reshape your data
+
+# relevant data manipulation packages:
+# tidyr
+# reshape2
+# dplyr
+# plyr
