7e52581a5f6cd0987281688f3ba23e80552fbf68
Xin Yuan <XinYuan1615@gmail.com>
2019-10-20 20:44:21 +0800

    Chapter 24

---
+++ Chapter 24 Do’s and don’ts of making effective graphics.R
@@ -0,0 +1,61 @@
+# 要点
+# *****************制作有效图的注意事项*****************
+# 怎么更好的解释数据，并不是复杂高级的图就好！！！
+
+# 主要函数:
+
+
+##### Chapter 24 Do’s and don’ts of making effective graphics #####
+
+#------ 24.1 Goal: create more effective graphs ------
+
+# effective graphs “improve understanding of data”
+# 有效图提高了理解数据的能力
+# One graph is more effective than another if its quantitative information 
+# can be decoded more quickly or more easily by most observers.
+
+#------ 24.2 No no’s ------
+
+#---- 24.2.1 Pie charts ----
+# 饼图:尽量不要使用
+
+# 我们最好能够通过物体在一个普通尺度上的位置进行比较，
+# 这就是为什么这些简单的点图比饼图更有效的原因。
+
+#---- 24.2.2 More pie charts ----
+
+#---- 24.2.3 Stacked and group bar charts ----
+# 堆叠和分组条形图
+# 不容易让人理解
+
+#---- 24.2.4 Self-contradiction ----
+# 自我矛盾
+# 图像和文字描述矛盾不可以
+
+#---- 24.2.5 Using Microsoft Excel to obscure your data and annoy your readers ----
+
+#------ 24.3 Do: make the data stand out ------
+# 让数据脱颖而出
+
+# less is more
+
+
+#------ 24.4 Do: spare your reader from mental gymnastics ------
+# 不要让读者绞尽脑汁
+
+#------ 24.5 Do: use position along a common scale ------
+# 
+
+#------ 24.6 Do: take control of aspect ratio ------
+# 控制长宽比
+
+#------ 24.7 Do: think about including zero ------
+
+#------ 24.8 Do: choose the scale with intention ------
+
+#------ 24.9 Do: connect the dots with care ------
+
+#------ 24.10 Do: convey groups clearly ------
+
+
+
