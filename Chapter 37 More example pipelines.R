1db7c372860db694dbf4da9dc57042da4ac06c1b
Xin Yuan <XinYuan1615@gmail.com>
2019-10-23 14:27:21 +0800

    Chapter 37

---
+++ Chapter 37 More example pipelines.R
@@ -0,0 +1,15 @@
+
+# https://stat545.com/example-pipelines.html
+# 更多Make的例子,等我换系统再试把
+
+##### Chapter 37 More example pipelines #####
+
+# There are three more toy pipelines, using the Lord of the Rings data, that reinforce:
+# 	
+# 01_automation-example_just-r - use of an R script as a pseudo-Makefile.
+
+# 02_automation-example_r-and-make - use of a simple Makefile.
+
+# 03_automation-example_render-without-rstudio - use of rmarkdown::render() from a 
+# Makefile, as the default way of running an R script or an R Markdown document, 
+# leading to pretty HTML reports without any mouse clicks.
